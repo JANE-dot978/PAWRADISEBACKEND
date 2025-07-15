e40d8735cab864a8f077969c2ae4290366b2c173
JANE-dot978 <gathujane97@gmail.com>
2025-07-16 00:41:57 +0300

    initial project setup, folder structure

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./helpers/initMongo"); // If you're using helpers/initMongo.js
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB(); 
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Default route to test server
+app.get("/", (req, res) => {
+  res.send("🐾 Welcome to Pawradise API");
+});
+
+// Load routes here later
+// app.use("/api/users", require("./routes/user.routes"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
