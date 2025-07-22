c9e00ea09b57264c4001d3e882398276eed552c7
JANE-dot978 <gathujane97@gmail.com>
2025-07-22 19:57:25 +0300

    add validation schema that prevents users from sending empty data

--- helpers/generateToken.js
+++ helpers/generateToken.js
@@ -1,1 +1,7 @@
-
+const joi =require('joi');
+const registerSchema = Joi.object({
+    name: Joi.string().required(),
+    email:Joi.string().required(),
+    password:Joi.string().min(6).required()
+});
+module.exports ={requreSchema};
